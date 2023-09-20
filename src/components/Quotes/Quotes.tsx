8b4ad1b35370f3936f7c27999e7cf12dcaabde84
Abdelrahman Ahmed <abdodawam2006@gmail.com>
2023-09-20 11:18:58 +0300

    update qoutes

--- src/components/Quotes/Quotes.tsx
+++ src/components/Quotes/Quotes.tsx
@@ -4,12 +4,14 @@ import './Quotes.css'
 const Qoutes = () => {
   return (
     <section className="qoutes">
-      <p className="text-[#B7903C]">"</p>
-      <h1>
-        Don't judge each day by the harvest you reap but by the seeds that you
-        plant.
-      </h1>
-      <h2> -Robert Louis Stevenson</h2>
+      <div className="container">
+        <p className="text-[#B7903C]">"</p>
+        <h1>
+          Don't judge each day by the harvest you reap but by the seeds that you
+          plant.
+        </h1>
+        <h2> -Robert Louis Stevenson</h2>
+      </div>
     </section>
   )
 }
